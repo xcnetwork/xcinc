74fe721f3e760305435871de2b0ac93d1d4bce05
xcnetwork9 <xcnetwork9@gmail.com>
2013-08-31 09:28:19 +0000

    q

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,7 @@
 // auto slider carousel custom
 
 function tinyCarouselGallery(json) {
-  document.write('<div id="tinycarousel"><div id="tinyarrow"><a class="buttons prev" href="#">' + tinyprevNav + '</a><a class="buttons next" href="#">' + tinynextNav + '</a><span>' + showText + ' ' + numposts_g + ' ' + postText + '</span></div><div class="viewport"><ul class="overview">');
+	document.write('<div id="tinycarousel"><div class="viewport"><ul class="overview">');
 	for (var i = 0; i < numposts_g; i++) {
 		var entry = json.feed.entry[i],
 			title = entry.title.$t,
@@ -28,10 +28,9 @@ function tinyCarouselGallery(json) {
 		var date_a = date.split('-')[2].substring(0, 2),
 			date_b = date.split('-')[1],
 			date_c = date.split('-')[0];
-
-
-    document.write('<li><div class="inner"><a href="' + link + '"' + (slideOpenNewTab ? ' target="_blank"' : '') + '><img src="http://xcinc.googlecode.com/svn/img/promo.png" id="promo"/><img src="' + img + '" alt="' + title + '" class="recent-thumb"></a><h6><a href="' + link + '"' + (slideOpenNewTab ? ' target="_blank"' : '') + '>' + title + '</a></h6><p>' + summ + '</p></div>' + (showPostDate_g ? '<em>' + date_a + ' ' + months[parseInt(date_b, 10)-1] + ' ' + date_c + '</em>' : '') + (showComm_g ? '<em>' + cm + ' ' + text + '</em>' : '') + '</li></ul></div></div>');
+		document.write('<li><div class="inner"><a href="' + link + '"' + (slideOpenNewTab ? ' target="_blank"' : '') + '><img src="http://xcinc.googlecode.com/svn/img/promo.png" id="promo"/><img src="' + img + '" alt="' + title + '" class="recent-thumb"></a><h6><a href="' + link + '"' + (slideOpenNewTab ? ' target="_blank"' : '') + '>' + title + '</a></h6><p>' + summ + '</p></div>' + (showPostDate_g ? '<em>' + date_a + ' ' + months[parseInt(date_b, 10)-1] + ' ' + date_c + '</em>' : '') + (showComm_g ? '<em>' + cm + ' ' + text + '</em>' : '') + '</li>');
 	}
+    document.write('</ul></div><div id="tinyarrow"><a class="buttons prev" href="#">' + tinyprevNav + '</a><a class="buttons next" href="#">' + tinynextNav + '</a><span>' + showText + ' ' + postText + '</span></div></div>');
 }
 
 document.write("<scr" + "ipt type='text/javascript' src='" + home_page.replace(/\/$/,"") + "/feeds/posts/summary/" + (slidebyLabels ? '-/' + slideLabelName : '') + "?max-results=" + numposts_g + "&orderby=published&alt=json-in-script&callback=tinyCarouselGallery'><\/scr" + "ipt>");
