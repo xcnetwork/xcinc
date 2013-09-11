3f292f932d8697a0f611a8d02eeed42c5fee7ff6
xcnetwork9@gmail.com <xcnetwork9@gmail.com@14606ad4-a6de-70f0-d458-e99b20eb9c07>
2013-09-11 08:07:38 +0000

    nn

---
+++ js/rc_thumb.js
@@ -0,0 +1,53 @@
+function RecentPostThumbnail(json) {
+// ul class
+document.write('<ul class="rc_thumb">'); for (var i = 0; i < numposts; i++) 
+
+{
+	var entry = json.feed.entry[i];var posttitle = entry.title.$t;var posturl;if (i == json.feed.entry.length) break;for (var k = 0; k < entry.link.length;k++){
+if(entry.link[k].rel=='replies'&&entry.link[k].type=='text/html'){var commenttext=entry.link[k].title;var commenturl=entry.link[k].href;}
+if (entry.link[k].rel == 'alternate') {posturl = entry.link[k].href;break;}
+}
+
+var thumburl;
+try {thumburl=entry.media$thumbnail.url;}
+catch (error)
+thumburl = thumburl.replace("/s72-c/","/s"+img_sz+"-c/");
+
+{
+	s=entry.content.$t;a=s.indexOf("<img");b=s.indexOf("src=\"",a);c=s.indexOf("\"",b+5);d=s.substr(b+5,c-b-5);if((a!=-1)&&(b!=-1)&&(c!=-1)&&(d!=""))
+{thumburl=d;} else thumburl=img_blank;}
+
+if(postthumbnails==true) 
+document.write('<li><a href="'+posturl+'"><img src="'+thumburl+'"/></a>');
+// post summary
+document.write('<div class="rc_sum"><h3>'+posttitle+'</h3>');
+
+    if ("content" in entry) {
+      var postcontent = entry.content.$t;}
+    else
+    if ("summary" in entry) {
+      var postcontent = entry.summary.$t;}
+    else var postcontent = "";
+    var re = /<\S[^>]*>/g; 
+    postcontent = postcontent.replace(re, "");
+
+if (postsummary == true) {
+
+      if (postcontent.length < numchars) {
+          document.write('<i>');
+         document.write(postcontent);
+          document.write('</i>');}
+      else {
+          document.write('<i>');
+         postcontent = postcontent.substring(0, numchars);
+         var quoteEnd = postcontent.lastIndexOf(" ");
+         postcontent = postcontent.substring(0,quoteEnd);
+         document.write(postcontent + '...');
+          document.write('</i>');}
+} 
+
+document.write('</div>'); // end post summary
+document.write('</li></ul>');
+} // end ul class
+
+document.write("<scr" + "ipt type='text/javascript' src='/feeds/posts/summary/" + (byLabels ? '-/' + LabelName : '') + "?max-results=" + numposts + "&orderby=published&alt=json-in-script&callback=RecentPostThumbnail'><\/scr" + "ipt>");
