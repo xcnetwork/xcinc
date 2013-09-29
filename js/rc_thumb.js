c85436d25f72d857d8a881bc86470a6eea851c8b
xcnetwork9 <xcnetwork9@gmail.com>
2013-09-29 09:35:49 +0000

    hh

--- js/rc_thumb.js
+++ js/rc_thumb.js
@@ -1,7 +1,7 @@
 // auto slider carousel custom
 
 function RecentThumbnail(json) {
-document.write('<div id="rc_label"><div class="rc_head"><h2>'+ judul +'</h2><span class="more_link"><a href="search/label/'+ judul +'?&amp;max-results=8">'+ judul +' '+ more +'</a></span></div><div class="viewport"><ul>');
+document.write('<div id="rc_label"><div class="rc_head"><h1>'+ LabelName +'</h1><span class="more_link"><a href="search/label/'+ LabelName +'?&amp;max-results=8">'+ LabelName +' '+ more +'</a></span></div><div class="viewport"><ul>');
         for (var i = 0; i < numposts; i++) {
                 var entry = json.feed.entry[i],
                         title = entry.title.$t,
@@ -25,7 +25,7 @@ document.write('<div id="rc_label"><div class="rc_head"><h2>'+ judul +'</h2><spa
                 img = ('media$thumbnail' in entry) ? entry.media$thumbnail.url : 'http://xcinc.googlecode.com/svn/img/noimage.png';
                 img = img.replace(/\/s[0-9]+(\-c)?\//, "/s"+img_sz+"-c/");
                                         
-document.write('<li><h3><a href="' + link + '">' + title + '</a></h3><a href="' + link + '"><img src="' + img + '" title="' + title + '"></a><p>' + summ + '</p></li>');
+document.write('<li><h3><a href="'+ link +'">'+ title +'</a></h3><a href="' + link + '"><img src="'+ img +'" title="'+ title +'"></a><p>'+ summ +'</p></li>');
         }
 document.write('</ul></div></div>');
 }
