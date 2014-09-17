eb2547173619d7f8c56468893ef5ae3ee583aff7
xcnetwork9 <xcnetwork9@gmail.com>
2014-09-17 16:57:56 +0000

    wd

--- js/rc_thumb.js
+++ js/rc_thumb.js
@@ -24,7 +24,7 @@ document.write('<div id="rc_label"><div class="rc_head"><h1>'+ LabelName +'</h1>
                 img = ('media$thumbnail' in entry) ? entry.media$thumbnail.url : 'http://xcinc.googlecode.com/svn/img/noimage.png';
                 img = img.replace(/\/s[0-9]+(\-c)?\//, "/s"+img_sz+"-c/");
                                         
-document.write('<li><h3><a href="'+ link +'">'+ title +'</a></h3><a href="' + link + '"><img src="'+ img +'"></a><p>'+ summ +'</p></li>');
+document.write('<li><h3><a href="'+ link +'">'+ title +'</a></h3><a href="' + link + '"><img src="'+ img +'"></a><p>'+ title +''+ summ +'</p></li>');
         }
 document.write('</ul></div></div>');
 }
