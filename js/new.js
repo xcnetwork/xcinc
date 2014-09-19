35ad0a41820b32c51b47367a9b56794a99833756
xcnetwork9@gmail.com <xcnetwork9@gmail.com@14606ad4-a6de-70f0-d458-e99b20eb9c07>
2014-09-19 15:43:07 +0000

    fdfd

--- js/new.js
+++ js/new.js
@@ -1,12 +1,11 @@
-
 // Recent Post Thumbnail
 
 function Terbaru(json) {
-document.write('<div id="rc_label"><div class="rc_head"><h1>'+ label +'</h1><span class="more_link"><a href="search/label/'+ label +'?&amp;max-results=8">'+ label +' '+ more +'</a></span></div><div class="viewport"><ul>');
+document.write('<div id="rc_label"><div class="rc_head"><h1>'+ label +'</h1></div><div class="viewport"><ul>');
         for (var i = 0; i < num_posts; i++) {
                 var entry = json.feed.entry[i],
                         title = entry.title.$t,
-                        link, summs, months, cm, img;
+                        link, summa, cm, img;
                 if (i == entry.length) break;
                 for (var j = 0, jen = entry.link.length; j < jen; j++) {
                         if (entry.link[j].rel == 'alternate') {
@@ -20,7 +19,7 @@ document.write('<div id="rc_label"><div class="rc_head"><h1>'+ label +'</h1><spa
                                 break;
                         }
                 }
-                summs = ("summary" in entry) ? entry.summary.$t.replace(/<(.*)?>/g, "") : "";
+                summa = ("summary" in entry) ? entry.summary.$t.replace(/<(.*)?>/g, "") : "";
                 summs = (summs.length > numchars) ? summs.substring(0, numchars) + '&hellip;' : summs;
                 img = ('media$thumbnail' in entry) ? entry.media$thumbnail.url : 'http://xcinc.googlecode.com/svn/img/noimage.png';
                 img = img.replace(/\/s[0-9]+(\-c)?\//, "/s"+img_sz+"-c/");
@@ -30,4 +29,4 @@ document.write('<li><a href="' + link + '"><img src="'+ img +'"><h3>'+ title +'<
 document.write('</ul></div></div>');
 }
 
-document.write("<scr" + "ipt type='text/javascript' src='/feeds/posts/summary/" + (byLabels ? '-/' + LabelName : '') + "?max-results=" + num_posts + "&orderby=published&alt=json-in-script&callback=Terbaru'><\/scr" + "ipt>");
+document.write("<scr" + "ipt type='text/javascript' src='/feeds/posts/summary/default'") + "?max-results=" + num_posts + "&orderby=published&alt=json-in-script&callback=Terbaru'><\/scr" + "ipt>");
