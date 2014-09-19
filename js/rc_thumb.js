c16ee9b47f6a77d03e9f2472c25e4caadc175827
xcnetwork9@gmail.com <xcnetwork9@gmail.com@14606ad4-a6de-70f0-d458-e99b20eb9c07>
2014-09-19 14:56:59 +0000

    hgfgh

--- js/rc_thumb.js
+++ js/rc_thumb.js
@@ -1,11 +1,11 @@
 // Recent Post Thumbnail
 
 function RecentThumbnail(json) {
-document.write('<div id="rc_label"><div class="rc_head"><h1>'+ LabelName +'</h1><span class="more_link"><a href="search/label/'+ LabelName +'?&amp;max-results=8">'+ LabelName +' '+ more +'</a></span></div><div class="viewport"><ul>');
+document.write('<div id="rc_label"><div class="rc_head"><h1>'+ label +'</h1><span class="more_link"><a href="search/label/'+ label +'?&amp;max-results=8">'+ label +' '+ more +'</a></span></div><div class="viewport"><ul>');
         for (var i = 0; i < num_posts; i++) {
                 var entry = json.feed.entry[i],
                         title = entry.title.$t,
-                        link, summ, months, cm, img;
+                        link, summs, months, cm, img;
                 if (i == entry.length) break;
                 for (var j = 0, jen = entry.link.length; j < jen; j++) {
                         if (entry.link[j].rel == 'alternate') {
@@ -19,8 +19,8 @@ document.write('<div id="rc_label"><div class="rc_head"><h1>'+ LabelName +'</h1>
                                 break;
                         }
                 }
-                summ = ("summary" in entry) ? entry.summary.$t.replace(/<(.*)?>/g, "") : "";
-                summ = (summ.length > numchars) ? summ.substring(0, numchars) + '&hellip;' : summ;
+                summs = ("summary" in entry) ? entry.summary.$t.replace(/<(.*)?>/g, "") : "";
+                summs = (summs.length > numchars) ? summs.substring(0, numchars) + '&hellip;' : summs;
                 img = ('media$thumbnail' in entry) ? entry.media$thumbnail.url : 'http://xcinc.googlecode.com/svn/img/noimage.png';
                 img = img.replace(/\/s[0-9]+(\-c)?\//, "/s"+img_sz+"-c/");
                                         
