2bb667aabed282477b3fdf208ef4ec393cbada0b
xcnetwork9@gmail.com <xcnetwork9@gmail.com@14606ad4-a6de-70f0-d458-e99b20eb9c07>
2014-09-19 15:55:22 +0000

    gfg

--- js/new.js
+++ js/new.js
@@ -1,11 +1,11 @@
 // Recent Post Thumbnail
 
-function RecentThumbnail(json) {
+function RecentPost(json) {
 document.write('<div id="rc_label"><div class="rc_head"><h1>'+ label +'</h1></div><div class="viewport"><ul>');
         for (var i = 0; i < num_posts; i++) {
                 var entry = json.feed.entry[i],
                         title = entry.title.$t,
-                        link, summs, months, cm, img;
+                        link, summs, cm, img;
                 if (i == entry.length) break;
                 for (var j = 0, jen = entry.link.length; j < jen; j++) {
                         if (entry.link[j].rel == 'alternate') {
@@ -22,11 +22,11 @@ document.write('<div id="rc_label"><div class="rc_head"><h1>'+ label +'</h1></di
                 summs = ("summary" in entry) ? entry.summary.$t.replace(/<(.*)?>/g, "") : "";
                 summs = (summs.length > numchars) ? summs.substring(0, numchars) + '&hellip;' : summs;
                 img = ('media$thumbnail' in entry) ? entry.media$thumbnail.url : 'http://xcinc.googlecode.com/svn/img/noimage.png';
-                img = img.replace(/\/s[0-9]+(\-c)?\//, "/s"+img_sz+"-c/");
+                img = img.replace(/\/s[0-9]+(\-c)?\//, "/s"+img_sz+");
                                         
 document.write('<li><a href="' + link + '"><img src="'+ img +'"><h3>'+ title +'</h3></a></li>');
         }
 document.write('</ul></div></div>');
 }
 
-document.write("<scr" + "ipt type='text/javascript' src='/feeds/posts/summary/" + (byLabels ? '-/' + label : '') + "?max-results=" + num_posts + "&orderby=published&alt=json-in-script&callback=RecentThumbnail'><\/scr" + "ipt>");
+document.write("<scr" + "ipt type='text/javascript' src='/feeds/posts/summary/" + (byLabels ? '-/' + label : '') + "?max-results=" + num_posts + "&orderby=published&alt=json-in-script&callback=RecentPost'><\/scr" + "ipt>");
